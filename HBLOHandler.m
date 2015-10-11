f86e1d8ebf1cb406879d4d8da6c4b98294382ab7
Adam Demasi <adam@hbang.com.au>
2015-10-11 13:23:53 +1030

    [HBLOHandler] add prefs bundle to the description

--- HBLOHandler.m
+++ HBLOHandler.m
@@ -10,7 +10,7 @@
 #pragma mark - NSObject
 
 - (NSString *)description {
-	return [NSString stringWithFormat:@"<%@: %p; name = %@; id = %@; prefs = %@>", self.class, self, self.name, self.identifier, self.preferencesClass];
+	return [NSString stringWithFormat:@"<%@: %p; name = %@; id = %@; prefs = %@ - %@>", self.class, self, self.name, self.identifier, self.preferencesBundle, self.preferencesClass];
 }
 
 @end
